2d2531506ae4491dbd3fd4ab8ba9e8d90a30194b
MAXIMUS ROSAS BURGOS <lianthg07@gmail.com>
2025-03-03 12:10:10 -0300

    Initial commit do Projeto da DIO SPRING

---
+++ src/main/java/meiiiii/dio/santaiiinder_dev_week/SantaiiinderDevWeekApplication.java
@@ -0,0 +1,13 @@
+package meiiiii.dio.santaiiinder_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaiiinderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaiiinderDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meiiiii/dio/santaiiinder_dev_week/SantaiiinderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package meiiiii.dio.santaiiinder_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaiiinderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
